90d9229902937202579ea9e42f0d0f6b3eea17e8
Dino Morelli <dino@ui3.info>
2022-09-18 10:18:19 -0400

    Partially working game logic
    This is hopelessly IO-bound and must be redesigned.

--- src/lib/Trapped/Game.hs
+++ src/lib/Trapped/Game.hs
@@ -1,13 +1,10 @@
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Trapped.Game
   ( startGame
   ) where
 
--- import Control.Monad ( replicateM )
--- import Lens.Micro.Platform ( makeLenses )
-import Lens.Micro.Platform
+import Lens.Micro.Platform ( (+~), (%~), (^.), makeLenses, to )
 import System.Random ( Random, randomRIO )
 
 
@@ -21,7 +18,7 @@ newtype FoodStores = FoodStores Int
   deriving (Eq, Num, Ord, Show)
 
 newtype Roll = Roll Int
-  deriving (Eq, Num, Random, Show)
+  deriving (Eq, Num, Ord, Random, Show)
 
 data GameData = GameData
   { _gdScandal :: Scandal
@@ -33,73 +30,109 @@ data GameData = GameData
 
 makeLenses ''GameData
 
+
+adjustScandal :: Scandal -> GameData -> GameData
+adjustScandal adj = gdScandal +~ adj
+
+
+adjustObsession :: Obsession -> GameData -> GameData
+adjustObsession adj = gdObsession +~ adj
+
+
 adjustFoodStores :: FoodStores -> GameData -> GameData
-adjustFoodStores fs gd = gd { gdFoodStores + fs }
+adjustFoodStores adj = gdFoodStores +~ adj
 
-initialState :: GameData
-initialState = GameData (Scandal 0) (Obsession 10) (FoodStores 10) []
 
-newtype Description = Description String
+initialData :: GameData
+initialData = GameData (Scandal 0) (Obsession 10) (FoodStores 10) []
+
+data State
+  = Playing GameData
+  | Ended
+  deriving Show
+
+newtype Message = Message String
   deriving Show
 
 
-gameContinuing :: GameData -> Either Description GameData
-gameContinuing gd
-  | gd ^. gdScandal >= Scandal 10 = Left (Description "The pressure and mockery from society reaches such heights that you decide to sell the house. Hans moves in after you leave and squats there.")
-  | gd ^. gdObsession < Obsession 1 = Left (Description "Hans finally loses interest in you and finds another unattainable person to chase. Victory. He does, however, write a thinly veiled short story about you. It's not flattering.")
-  | gd ^. gdFoodStores < FoodStores 1 = Left (Description "You run out of food and starve before Hans relents. The funeral is tasteful. He does not attend.")
-  | otherwise = Right gd
+gameContinuing :: State -> IO State
+gameContinuing st@Ended = pure st
+gameContinuing st@(Playing gd)
+  | gd ^. gdScandal >= Scandal 10 = displayMsg (Message "The pressure and mockery from society reaches such heights that you decide to sell the house. Hans moves in after you leave and squats there.") >> pure Ended
+  | gd ^. gdObsession < Obsession 1 = displayMsg (Message "Hans finally loses interest in you and finds another unattainable person to chase. Victory. He does, however, write a thinly veiled short story about you. It's not flattering.") >> pure Ended
+  | gd ^. gdFoodStores < FoodStores 1 = displayMsg (Message "You run out of food and starve before Hans relents. The funeral is tasteful. He does not attend.") >> pure Ended
+  | otherwise = pure st
 
 
-checkFiveWeekRule :: GameData -> Either String GameData
-checkFiveWeekRule gd =
-  if all (== Roll 5) . take 3 $ gd ^. gdRollHistory
-    then Left "Victory. Hans is dragged away kicking and screaming by an apologetic relative, nurse or member of the constabulary."
-    else Right gd
+hasItBeenFiveWeeks :: [Roll] -> Bool
+hasItBeenFiveWeeks l = length l >= 3 && (all (== Roll 5) . take 3 $ l)
 
 
-getRoll :: IO Roll
-getRoll = randomRIO (Roll 1, Roll 6)
+rollDie :: State -> IO State
+rollDie st@Ended = pure st
+rollDie (Playing gd) = do
+  newRoll <- randomRIO (Roll 1, Roll 6)
+  let newData = gdRollHistory %~ (newRoll :) $ gd
+  if hasItBeenFiveWeeks $ newData ^. gdRollHistory
+    then displayMsg (Message "Victory. Hans is dragged away kicking and screaming by an apologetic relative, nurse or member of the constabulary.") >> pure Ended
+    else pure $ Playing newData
 
 
--- adjustState :: _ -> GameData -> GameData
-
-{- Each roll starts a series of look ups in the tables
-
-   First in the Hans On Event... table, which direct to one of the other three
-   Desperation Mounts..., Peeking Through The Curtains..., Hans' Antics...
--}
+-- FIXME
+ynChoice :: Message -> IO Bool
+ynChoice (Message s) = do
+  putStrLn s
+  pure True
 
 
-data Adjustment = Adjustment Description [GameData -> GameData]
+displayMsg :: Message -> IO ()
+displayMsg (Message s) = putStrLn s
 
 
-makeChoice :: Description -> IO Bool
+evalEvent :: State -> IO State
+
+evalEvent st@Ended = pure st
+
+evalEvent st@(Playing gd)
+  | gd ^. gdRollHistory . to head <= Roll 2 = rollDie st >>= desperationLookup
+  | otherwise = rollDie st >>= desperationLookup
 
 
-hansEventLookup = \case
-  1 ->
-  2 -> desperationLookup
-
-desperationLookup :: Roll -> IO LookupAction
-desperationLookup = \case
-  Roll 1 -> pure . Adjustment
-    (Description "Rats. Rats in your basement")
-    [ gdFoodStores %~ (subtract (FoodStores 1)) ]
-  Roll 2 -> pure . Adjustment
-    (Description "Your dogs have eyes as big as saucers. Or dinner plates.")
-    [ gdFoodStores %~ (subtract (FoodStores 2)) ]
-  Roll 3 -> do
-    eatTheDuckling <- makeChoice $ Description "You find an ugly duckling. It's sad. Do you eat it?"
-    pure . if eatTheDuckling
-      then Adjustment (Description "That was delicious!") [ adjustFoodStores +1 ]
-      else Adjustment (Description "Another mouth to feed!") [ adjustFoodStores -1 ]
-    Choice (Description "You find an ugly duckling. It's sad. Do you eat it?")
-  _ -> Adjustment (Description "not yet implemented") []
+desperationLookup :: State -> IO State
+desperationLookup st@Ended = pure st
+desperationLookup (Playing gd) = do
+  Playing <$> case gd ^. gdRollHistory . to head of
+    Roll 1 -> do
+      displayMsg (Message "Rats. Rats in your basement")
+      pure $ (adjustFoodStores $ FoodStores (-1)) gd
+    Roll 2 -> do
+      displayMsg (Message "Your dogs have eyes as big as saucers. Or dinner plates.")
+      pure $ (adjustFoodStores $ FoodStores (-2)) gd
+    Roll 3 -> do
+      eatTheDuckling <- ynChoice $ Message "You find an ugly duckling. It's sad. Do you eat it?"
+      if eatTheDuckling
+        then displayMsg (Message "That was delicious!") >> (pure $ (adjustFoodStores $ FoodStores 1) gd)
+        else displayMsg (Message "Another mouth to feed.") >> (pure $ (adjustFoodStores $ FoodStores (-1)) gd)
+    Roll 4 -> do
+      displayMsg (Message "You write letters to your friends. No help comes.")
+      pure $ (adjustScandal $ Scandal 1) gd
+    Roll 5 -> displayMsg (Message "You play solitaire.") >> pure gd
+    Roll 6 -> do
+      displayMsg (Message "More of your food spoils. How long can this go on?")
+      pure $ (adjustFoodStores $ FoodStores (-1)) gd
+    _ -> displayMsg (Message "not yet implemented") >> pure gd
 
 
 startGame :: IO ()
-startGame = do
-  print $ gameContinuing initialState
-  print . gameContinuing . (gdScandal .~ Scandal 10) $ initialState
-  -- print =<< replicateM 6 getRoll
+startGame = gameLoop . Playing $ initialData
+
+
+gameLoop :: State -> IO ()
+
+gameLoop st@(Playing _) = do
+  putStrLn "---"
+  newState <- rollDie st >>= evalEvent >>= gameContinuing
+  print newState
+  gameLoop newState
+
+gameLoop Ended = pure ()
