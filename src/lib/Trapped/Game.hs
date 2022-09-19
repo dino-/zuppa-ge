7dde3498f1b8398064962b855cac848b4f3d4783
Dino Morelli <dino@ui3.info>
2022-09-19 17:07:43 -0400

    Refined the display to fit the "turns" better
    Removed some unused code. Fixed a couple of bugs in the stats adjustment
    code.

--- src/lib/Trapped/Game.hs
+++ src/lib/Trapped/Game.hs
@@ -75,12 +75,18 @@ evalGame SInitGame (ENewTurn roll) =
   pure . SStartTurn . (gdRollHistory %~ (roll :)) $ initialData
 
 evalGame (SStartTurn gd) (EDesperation roll) = do
+  display "---"
+  display "Desperation mounts..."
   pure . SDesperation . (gdRollHistory %~ (roll :)) $ gd
 
 evalGame (SStartTurn gd) (EPeeking roll) = do
+  display "---"
+  display "Peeking through the curtains..."
   pure . SPeeking . (gdRollHistory %~ (roll :)) $ gd
 
 evalGame (SStartTurn gd) (EAntics roll) = do
+  display "---"
+  display "Hans' antics..."
   pure . SAntics . (gdRollHistory %~ (roll :)) $ gd
 
 evalGame (SDesperation gd) (ENewTurn roll) = do
@@ -100,26 +106,19 @@ evalGame _ EEndConditions = pure SEnded
 evalGame state _ = pure state
 
 
-withLogging :: (Show s, Show e) => FSM s e -> FSM s e
-withLogging fsm s e = do
-  s' <- fsm s e
-  printf "---\n%s x %s -> %s\n" (show s) (show e) (show s')
-  return s'
+display :: String -> IO ()
+display = putStrLn
 
 
-newtype Message = Message String
-  deriving Show
-
-
-displayMsg :: Message -> GameData -> IO ()
-displayMsg (Message s) (GameData (Scandal scandal) (Obsession obsession) (FoodStores foodStores) _) =
-  printf "---\n%s\nScandal: %2d  Obsession: %2d  Food stores: %2d\n"
-    s scandal obsession foodStores
+displayGameData :: GameData -> IO ()
+displayGameData (GameData (Scandal scandal) (Obsession obsession) (FoodStores foodStores) _) =
+  printf "Scandal: %2d  Obsession: %2d  Food stores: %2d\n"
+    scandal obsession foodStores
 
 
 -- FIXME
-ynChoice :: Message -> IO Bool
-ynChoice (Message s) = do
+ynChoice :: String -> IO Bool
+ynChoice s = do
   putStrLn s
   pure True
 
@@ -129,25 +128,26 @@ desperationLookup oldGd roll = do
   let gd = gdRollHistory %~ (roll :) $ oldGd
   (msg, adjustedGd) <- case roll of
     Roll 1 -> pure
-      ( Message "Rats. Rats in your basement"
+      ( "Rats. Rats in your basement"
       , adjustFoodStores (-1) gd )
     Roll 2 -> pure
-      ( Message "Your dogs have eyes as big as saucers. Or dinner plates."
+      ( "Your dogs have eyes as big as saucers. Or dinner plates."
       , adjustFoodStores (-2) gd )
     Roll 3 -> do
-      eatTheDuckling <- ynChoice $ Message "You find an ugly duckling. It's sad. Do you eat it?"
+      eatTheDuckling <- ynChoice $ "You find an ugly duckling. It's sad. Do you eat it?"
       pure $ if eatTheDuckling
-        then (Message "That was delicious!", adjustFoodStores 1 gd)
-        else (Message "Another mouth to feed.", adjustFoodStores (-1) gd)
+        then ("That was delicious!", adjustFoodStores 1 gd)
+        else ("Another mouth to feed.", adjustFoodStores (-1) gd)
     Roll 4 -> pure
-      ( Message "You write letters to your friends. No help comes."
+      ( "You write letters to your friends. No help comes."
       , adjustScandal 1 gd )
-    Roll 5 -> pure (Message "You play solitaire.", gd)
+    Roll 5 -> pure ("You play solitaire.", gd)
     Roll 6 -> pure
-      ( Message "More of your food spoils. How long can this go on?"
+      ( "More of your food spoils. How long can this go on?"
       , adjustFoodStores (-1) gd )
-    _ -> pure (Message "not yet implemented", gd)
-  displayMsg msg adjustedGd
+    _ -> pure ("not yet implemented", gd)
+  display msg
+  displayGameData adjustedGd
   pure adjustedGd
 
 
@@ -156,25 +156,26 @@ peekingLookup oldGd roll = do
   let gd = gdRollHistory %~ (roll :) $ oldGd
   let (msg, adjustedGd) = case roll of
         Roll 1 ->
-          ( Message "A little matchstick girl dies on your lawn. In front of him."
+          ( "A little matchstick girl dies on your lawn. In front of him."
           , (adjustObsession (-1)) . (adjustScandal 1) $ gd )
         Roll 2 ->
-          ( Message "He's having a portrait done of himself. On your LAWN."
+          ( "He's having a portrait done of himself. On your LAWN."
           , adjustScandal 1 gd )
         Roll 3 ->
-          ( Message "He's hired a small orchestra to blast music at your house."
+          ( "He's hired a small orchestra to blast music at your house."
           , adjustScandal 2 gd )
         Roll 4 ->
-          ( Message "A new face wanders past."
+          ( "A new face wanders past."
           , adjustObsession (-1) gd )
         Roll 5 ->
-          ( Message "He's talking to your neighbours about you."
+          ( "He's talking to your neighbours about you."
           , adjustObsession 1 gd )
         Roll 6 ->
-          ( Message "He catches a glimpse of you."
-          , adjustFoodStores 1 gd )
-        _ -> (Message "not yet implemented", gd)
-  displayMsg msg adjustedGd
+          ( "He catches a glimpse of you."
+          , adjustObsession 1 gd )
+        _ -> ("not yet implemented", gd)
+  display msg
+  displayGameData adjustedGd
   pure adjustedGd
 
 
@@ -183,25 +184,26 @@ anticsLookup oldGd roll = do
   let gd = gdRollHistory %~ (roll :) $ oldGd
   let (msg, adjustedGd) = case roll of
         Roll 1 ->
-          ( Message "He's naked. He claims the clothes are visible."
+          ( "He's naked. He claims the clothes are visible."
           , adjustScandal 2 gd )
         Roll 2 ->
-          ( Message "He's shoving letters through your door. Stack of them."
+          ( "He's shoving letters through your door. Stack of them."
           , adjustObsession (-1) gd )
         Roll 3 ->
-          ( Message "He's weeping and howling, rolling about on the grass."
+          ( "He's weeping and howling, rolling about on the grass."
           , adjustScandal 1 . adjustObsession (-1) $ gd )
         Roll 4 ->
-          ( Message "He stamps on a parcel intended for you."
+          ( "He stamps on a parcel intended for you."
           , adjustFoodStores (-1) gd )
         Roll 5 ->
-          ( Message "He's punching the house and crying about it."
+          ( "He's punching the house and crying about it."
           , adjustObsession (-1) gd )
         Roll 6 ->
-          ( Message "He puts his mouth to the letterbox and screams."
+          ( "He puts his mouth to the letterbox and screams."
           , adjustScandal 1 gd )
-        _ -> (Message "not yet implemented", gd)
-  displayMsg msg adjustedGd
+        _ -> ("not yet implemented", gd)
+  display msg
+  displayGameData adjustedGd
   pure adjustedGd
 
 
@@ -219,10 +221,26 @@ hasItBeenFiveWeeks rolls = length rolls >= 3 && (all (== Roll 5) . take 3 $ roll
 
 endConditionsMet :: GameData -> IO Bool
 endConditionsMet gd
-  | gd ^. gdScandal >= Scandal 10 = displayMsg (Message "The pressure and mockery from society reaches such heights that you decide to sell the house. Hans moves in after you leave and squats there.") gd >> pure True
-  | gd ^. gdObsession < Obsession 1 = displayMsg (Message "Hans finally loses interest in you and finds another unattainable person to chase. Victory. He does, however, write a thinly veiled short story about you. It's not flattering.") gd >> pure True
-  | gd ^. gdFoodStores < FoodStores 1 = displayMsg (Message "You run out of food and starve before Hans relents. The funeral is tasteful. He does not attend.") gd >> pure True
-  | hasItBeenFiveWeeks $ gd ^. gdRollHistory = displayMsg (Message "After five harrowing weeks, victory. Hans is dragged away kicking and screaming by an apologetic relative, nurse or member of the constabulary.") gd >> pure True
+  | gd ^. gdScandal >= Scandal 10 = do
+    display "---"
+    display "The pressure and mockery from society reaches such heights that you decide to sell the house. Hans moves in after you leave and squats there."
+    displayGameData gd
+    pure True
+  | gd ^. gdObsession < Obsession 1 = do
+    display "---"
+    display "Hans finally loses interest in you and finds another unattainable person to chase. Victory. He does, however, write a thinly veiled short story about you. It's not flattering."
+    displayGameData gd
+    pure True
+  | gd ^. gdFoodStores < FoodStores 1 = do
+    display "---"
+    display "You run out of food and starve before Hans relents. The funeral is tasteful. He does not attend."
+    displayGameData gd
+    pure True
+  | hasItBeenFiveWeeks $ gd ^. gdRollHistory = do
+    display "---"
+    display "After five harrowing weeks, victory. Hans is dragged away kicking and screaming by an apologetic relative, nurse or member of the constabulary."
+    displayGameData gd
+    pure True
   | otherwise = pure False
 
 
@@ -264,7 +282,6 @@ gameLoop state = do
     SEnded -> pure Nothing
 
   maybe (pure ()) (\event -> do
-    -- newState <- (withLogging evalGame) state event
     newState <- evalGame state event
     gameLoop newState)
     mevent
